713c12f3de69ca1eed22a917df2cf04a1fce876c
Chenchukrishnamachari <chenchu.tirakanam@gmail.com>
2020-11-16 20:44:31 +0530

    Added FactsListActivity.

---
+++ app/src/main/java/com/wipro/factsapp/FactsListActivity.kt
@@ -0,0 +1,11 @@
+package com.wipro.factsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FactsListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_facts_list)
+    }
+}
